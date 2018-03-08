542ea1a352cb15ea5ea6361e8464cecb8e8ffada
Thomas Lombart <t.lombart97@gmail.com>
2018-03-08 14:21:35 +0100

    Add footer

--- client/src/components/layout/Footer/Footer.js
+++ client/src/components/layout/Footer/Footer.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './Footer.css'
+import './Footer.css';
 
 const Footer = () => (
-    <div>
-        <h1>Footer</h1>
-    </div>
+  <div className="footer-wrapper">
+      <p className="footer-text">Coded by Ayumi Saito, Fred Hawk, Thomas Lombart and Mario Krajacic</p>
+    </div >
 );
 
 export default Footer;
